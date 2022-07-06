9075ff7642f07a73921f3f33b2d94157f86e5e45
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-07-06 09:23:53 +0200

    module_00/ex00

--- module_00/ex00/megaphone.cpp
+++ module_00/ex00/megaphone.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
 #include <string>
 
-int main(int argc, char *argv[]) {
-  if (argc > 0) {
-    if (argc == 1) {
-      std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-    } else {
-      for (int i = 1; i < argc; ++i) {
-        std::string str(argv[i]);
-        for (std::string::iterator it = str.begin(); it != str.end(); ++it) {
-          std::cout << static_cast<char>(std::toupper(*it));
-        }
+int main(int argc, char* argv[]) {
+  if (argc == 1) {
+    std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+  } else if (argc > 1) {
+    for (int i = 1; i < argc; ++i) {
+      std::string str(argv[i]);
+      for (int j = 0; str[j] != 0; ++j) {
+        std::cout << static_cast<char>(std::toupper(str[j]));
       }
-      std::cout << std::endl;
     }
-    return 0;
+    std::cout << std::endl;
   }
-  return 1;
 }

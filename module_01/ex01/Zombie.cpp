45f230c66f65896a9212695d883fa35c6937a5ee
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-07-06 10:06:18 +0200

    module_01/ex01

--- module_01/ex01/Zombie.cpp
+++ module_01/ex01/Zombie.cpp
@@ -6,10 +6,14 @@ void Zombie::announce() {
   std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
 }
 
-void Zombie::SetName(std::string new_name) { name = new_name; }
+void Zombie::SetName(std::string new_name) {
+  name = new_name;
+}
 
 Zombie::Zombie(std::string name) : name(name) {}
 
 Zombie::Zombie() {}
 
-Zombie::~Zombie() { std::cout << name << ": dead..." << std::endl; }
+Zombie::~Zombie() {
+  std::cout << name << ": dead..." << std::endl;
+}

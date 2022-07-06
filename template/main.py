41ea7282e979a3fbab8bcb9199712b285c2e9ae3
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-07-06 08:56:02 +0200

    Update template

--- template/main.py
+++ template/main.py
@@ -1,3 +1,4 @@
+from array import array
 from string import Template
 import shutil
 import argparse
@@ -6,8 +7,13 @@ import os
 parser = argparse.ArgumentParser()
 parser.add_argument("dst")
 parser.add_argument("name")
-parser.add_argument("files")
+parser.add_argument('files', nargs=argparse.REMAINDER)
 args = parser.parse_args()
+
+files = []
+for f in args.files:
+  f.replace(',', '')
+  files.append(f)
 
 src = os.path.abspath(os.path.dirname(__file__))
 
@@ -21,7 +27,7 @@ with open(os.path.join(src, ".gitignore"), "r") as gitignore:
 
 with open(os.path.join(src, "Makefile"), "r") as makefile:
   template = Template(makefile.read())
-  template = template.safe_substitute(project_name=args.name, project_files=args.files)
+  template = template.safe_substitute(project_name=args.name, project_files=" ".join(files))
   with open(os.path.join(args.dst, "Makefile"), "w") as new_makefile:
     new_makefile.write(template)
 
@@ -29,6 +35,5 @@ shutil.copyfile(os.path.join(src, ".clang-format"), os.path.join(args.dst, ".cla
 shutil.copyfile(os.path.join(src, ".clang-tidy"), os.path.join(args.dst, ".clang-tidy"))
 shutil.copyfile(os.path.join(src, ".clangd"), os.path.join(args.dst, ".clangd"))
 
-files = args.files.split(" ")
 for f in files:
   open(os.path.join(args.dst,f), 'w')

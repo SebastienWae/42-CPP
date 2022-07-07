dd5d4eb53e506a296ab551fe09e8f0708f064af7
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-07-07 16:30:01 +0200

    module_03/ex00

---
+++ module_03/ex00/ClapTrap.cpp
@@ -0,0 +1,79 @@
+
+#include "ClapTrap.h"
+#include <iostream>
+#include <limits>
+#include <string>
+
+ClapTrap::ClapTrap(std::string name)
+    : name(name), hit_points(10), energy_points(10), attack_damage(0) {
+  std::cout << "A new ClapTrap named " << name << " was created!" << std::endl;
+}
+
+ClapTrap::ClapTrap(ClapTrap& other) {
+  name = other.name;
+  hit_points = other.hit_points;
+  energy_points = other.energy_points;
+  attack_damage = other.attack_damage;
+  std::cout << "ClapTrap " << name << " was duplicated." << std::endl;
+}
+
+ClapTrap& ClapTrap::operator=(const ClapTrap& copy) {
+  name = copy.name;
+  hit_points = copy.hit_points;
+  energy_points = copy.energy_points;
+  attack_damage = copy.attack_damage;
+  std::cout << "ClapTrap " << name << " was copied." << std::endl;
+  return *this;
+}
+
+ClapTrap::~ClapTrap() {
+  std::cout << "ClapTrap " << name << " was destroyed!" << std::endl;
+}
+
+void ClapTrap::attack(const std::string& target) {
+  if (hit_points == 0) {
+    std::cout << "ClapTrap " << name << " cannot attack " << target
+              << ". It's broken." << std::endl;
+  } else if (energy_points == 0) {
+    std::cout << "ClapTrap " << name << " cannot attack " << target
+              << ". It's out of energy." << std::endl;
+  } else {
+    std::cout << "ClapTrap " << name << " attacks " << target << ", causing "
+              << attack_damage << " points of "
+              << "damage!" << std::endl;
+    energy_points--;
+  }
+}
+
+void ClapTrap::takeDamage(unsigned int amount) {
+  if (hit_points == 0) {
+    std::cout << "Stop attacking ClapTrap " << name << " it's already broken!"
+              << std::endl;
+  } else {
+    std::cout << "ClapTrap " << name << " took " << amount
+              << " points of damage!" << std::endl;
+    if (amount >= hit_points) {
+      std::cout << "ClapTrap " << name << " is out of service." << std::endl;
+      hit_points = 0;
+    } else {
+      hit_points -= amount;
+    }
+  }
+}
+
+void ClapTrap::beRepaired(unsigned int amount) {
+  if (hit_points == 0) {
+    std::cout << "ClapTrap " << name << " cannot be repaired." << std::endl;
+  } else if (energy_points == 0) {
+    std::cout << "ClapTrap " << name
+              << " doesn't have enough energy to repair itself." << std::endl;
+  } else {
+    if (amount >= std::numeric_limits<unsigned int>::max() - hit_points) {
+      amount = std::numeric_limits<unsigned int>::max() - hit_points;
+    }
+    std::cout << "ClapTrap " << name << " repaired itself of " << amount
+              << " hit points." << std::endl;
+    energy_points--;
+    hit_points += amount;
+  }
+}
---
+++ module_03/ex00/main.cpp
@@ -0,0 +1,34 @@
+#include <limits>
+#include "ClapTrap.h"
+
+int main(void) {
+  ClapTrap c1("c1");
+  ClapTrap c2(c1);
+  ClapTrap c3 = c2;
+
+  c1.attack("target 1");
+  c1.attack("");
+
+  c1.takeDamage(9);
+
+  c1.beRepaired(2);
+
+  c1.beRepaired(std::numeric_limits<unsigned int>::min());
+  c1.beRepaired(std::numeric_limits<unsigned int>::max());
+
+  c1.attack("target 1");
+  c1.attack("target 2");
+  c1.attack("target 1");
+  c1.attack("target 2");
+  c1.attack("target 1");
+
+  c1.attack("target 1");
+  c1.beRepaired(10);
+
+  c1.takeDamage(9);
+  c1.takeDamage(std::numeric_limits<unsigned int>::max());
+  c1.takeDamage(1);
+
+  c2.takeDamage(std::numeric_limits<unsigned int>::min());
+  c3.takeDamage(std::numeric_limits<unsigned int>::max());
+}

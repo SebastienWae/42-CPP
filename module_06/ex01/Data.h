d46006b5bacd468a5ea8792cd40d48416ba7e76d
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-07-13 17:52:54 +0200

    save

---
+++ module_06/ex01/Data.h
@@ -0,0 +1,11 @@
+#ifndef DATA_H
+#define DATA_H
+
+struct Data {
+  int i;
+  char c;
+
+  Data(int i, char c);
+};
+
+#endif
---
+++ module_06/ex01/main.cpp
@@ -0,0 +1,22 @@
+#include <cstdint>
+#include <iostream>
+#include <system_error>
+
+#include "Data.h"
+
+uintptr_t serialize(Data* ptr) { return reinterpret_cast<std::uintptr_t>(ptr); }
+
+Data* deserialize(uintptr_t raw) { return reinterpret_cast<Data*>(raw); }
+
+Data::Data(int i, char c) : i(i), c(c) {}
+
+int main(void) {
+  Data d(1, 'c');
+
+  uintptr_t d_ser = serialize(&d);
+  std::cout << d_ser << std::endl;
+  Data* d_ptr = deserialize(d_ser);
+  std::cout << d_ptr << std::endl;
+  std::cout << "i: " << d_ptr->i << std::endl;
+  std::cout << "c: " << d_ptr->c << std::endl;
+}

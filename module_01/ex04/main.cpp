ba259df25403b43c5fa13e4c4ae485c5cc48702e
Sebastien Waegeneire <sebastien@waegeneire.com>
2022-07-14 09:30:33 +0200

    save

--- module_01/ex04/main.cpp
+++ module_01/ex04/main.cpp
@@ -3,38 +3,43 @@
 #include <string>
 
 int main(int argc, char* argv[]) {
-  if (argc == 4) {
-    std::string line;
-
-    std::string old_str = argv[2];
-    std::string new_str = argv[3];
-
-    std::ifstream old_file(argv[1]);
-    if (!old_file.is_open()) {
-      std::cout << "Unable to open file" << std::endl;
-      return 1;
-    }
-
-    std::string new_file_name = static_cast<std::string>(argv[1]).append(".replace");
-    std::ofstream new_file(new_file_name.c_str());
-    if (!new_file.is_open()) {
-      std::cout << "Unable to open file" << std::endl;
-      old_file.close();
-      return 1;
-    }
-
-    while (std::getline(old_file, line) != 0) {
-      std::string::size_type start = line.find(old_str);
-      while (start != std::string::npos) {
-        new_file << line.substr(0, start);
-        new_file << new_str;
-        line = line.substr(start + old_str.length());
-        start = line.find(old_str);
-      }
-      new_file << line << std::endl;
-    }
-    return 0;
+  if (argc != 4) {
+    std::cerr << "usage: sed_is_for_losers file s1 s2" << std::endl;
+    return 1;
   }
-  std::cout << "Invalid arguments" << std::endl;
-  return 1;
+
+  std::string input_file_name = argv[1];
+  std::string const output_file_name = input_file_name + ".replace";
+  std::string old_string = argv[2];
+  std::string new_string = argv[3];
+
+  if (old_string.empty()) {
+    std::cerr << "cannot replace empty string" << std::endl;
+    return 1;
+  }
+
+  std::ifstream input_file;
+  input_file.open(input_file_name.c_str(), std::ifstream::in);
+  if (input_file.fail()) {
+    std::cerr << "failed to open " << input_file_name << " for reading" << std::endl;
+    return 1;
+  }
+
+  std::ofstream output_file;
+  output_file.open(output_file_name.c_str(), std::ofstream::out);
+  if (output_file.fail()) {
+    std::cerr << "failed to open " << output_file_name << " for writing" << std::endl;
+    return 1;
+  }
+
+  std::string line;
+  while (getline(input_file, line) != 0) {
+    std::string::size_type n;
+    while ((n = line.find(old_string)) != std::string::npos) {
+      output_file << line.substr(0, n);
+      line.erase(0, n + old_string.length());
+      output_file << new_string;
+    }
+    output_file << line << std::endl;
+  }
 }
